48900e42205db123551e1c27c7cce4154874bf57
Vitold S <vit1251@gmail.com>
2018-12-20 07:31:26 +0300

    Initial revision

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vit1251/beego_pt
+
+require github.com/astaxie/beego v1.11.1
---
+++ main.go
@@ -0,0 +1,44 @@
+package beego_pt
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"log/syslog"
+	"fmt"
+)
+
+var customAddr string
+var customService string
+var customWriter *syslog.Writer
+
+//func InitReport(ctx *context.Context) {
+//}
+
+func ProcessReport(ctx *context.Context) {
+	msg := fmt.Sprintf("Request complete")
+	customWriter.Write([]byte(msg))
+}
+
+// InitPaperTrailFilter("logs4.papertrailapp.com:49763", "znzn")
+func InitPaperTrailFilter(addr string, service string) (error) {
+
+	// Initialize PaperTrail parameters
+	customAddr = addr
+	customService = service
+
+	// Initialize PaperTrail writer
+	w, err := syslog.Dial("udp", addr, syslog.LOG_EMERG | syslog.LOG_KERN, service)
+	if err != nil {
+		return err
+	}
+	customWriter = w
+
+	// Initialize BeeGo request handler
+//	beego.InsertFilter("*", beego.BeforeRouter, InitReport)
+	beego.InsertFilter("*", beego.FinishRouter, ProcessReport, false)
+
+	// Complete report
+	beego.Info("PaperTrail BeeGo reporter system initialized")
+
+	return nil
+}
